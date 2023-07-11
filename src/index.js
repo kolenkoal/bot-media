5047177433543ca82d924c975fdbe24ba91eea45
Alexander Kolenko <kolenko.alexader0@gmail.com>
2023-07-11 10:03:56 +0300

    Added several error handlers. Also,  several message have been added to the console reporting user actions

--- src/index.js
+++ src/index.js
@@ -18,19 +18,28 @@ const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
 
 // Bot start
 bot.onText(/\/start/, async (msg) => {
-    const chatId = msg.chat.id
-    console.log('Bot was started');
-    await bot.sendMessage(chatId, startMessage, typeOfCardButtons)
+    try {
+        const chatId = msg.chat.id
+        console.log(`The bot was started in ${chatId} chat`);
+        await bot.sendMessage(chatId, startMessage, typeOfCardButtons)
+
+    } catch (err) {
+        console.error(err);
+    }
+
 })
 
 //User interaction
 bot.on('callback_query', async (query) => {
-    const chatId = query.message.chat.id;
+    const chatId = query.message.chat.id,
+        date = new Date();
+
     try {
         if (['3', '5', '10', '20'].includes(query.data)) {
             const amount = Number(query.data) * 100,
                 title = 'Donation',
                 description = `Поддержать «Холод» на ${amount / 100} €`,
+                payload = `ChatID: ${chatId}. Date: ${date}. Amount: ${amount / 100} €`,
                 providerToken = process.env.PROVIDER_TOKEN,
                 currency = 'EUR',
                 prices = [{
@@ -42,13 +51,17 @@ bot.on('callback_query', async (query) => {
                 chatId,
                 title,
                 description,
-                'СДЕЛАТЬ',
+                payload,
                 providerToken,
                 currency,
                 prices)
+
+            console.log(payload)
         }
+
         switch (query.data) {
             case 'Foreign Card':
+                console.log(`A foreign Card was chosen in ${chatId} chat`)
                 await bot.sendMessage(chatId, chooseAmountMessage, amountButtons)
                 break;
         }
@@ -60,14 +73,32 @@ bot.on('callback_query', async (query) => {
 
 // Pre-checkout
 bot.on('pre_checkout_query', async (query) => {
-    await bot.answerPreCheckoutQuery(query.id, true);
-})
+    const chatId = query.from.id;
 
+    try {
+        console.log(`Pre-Checkout Query in ${chatId} chat`)
+        await bot.answerPreCheckoutQuery(query.id, true);
+
+    } catch (err) {
+        console.error(err)
+    }
+})
 
 // Successful payment
 bot.on('successful_payment', async (msg) => {
-    await bot.sendMessage(msg.chat.id, successfulPaymentMessage);
+    const chatId = msg.from.id;
+
+    try {
+        console.log(`Successful payment in ${chatId} chat`)
+        await bot.sendMessage(msg.chat.id, successfulPaymentMessage);
+
+    } catch (err) {
+        console.error(err)
+    }
 })
+
+// Handling errors
+bot.on('error', msg => console.error(msg))
 
 // Error while starting a bot
 bot.on('polling_error', (err) => {
